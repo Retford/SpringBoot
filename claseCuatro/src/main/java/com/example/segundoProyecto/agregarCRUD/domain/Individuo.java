2bd4d74af4c1974ad966c8e44a13741b225762e1
Retford <retfordjb@gmail.com>
2023-09-09 19:34:01 -0500

    add language

--- claseCuatro/src/main/java/com/example/segundoProyecto/agregarCRUD/domain/Individuo.java
+++ claseCuatro/src/main/java/com/example/segundoProyecto/agregarCRUD/domain/Individuo.java
@@ -1,6 +1,8 @@
 package com.example.segundoProyecto.agregarCRUD.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -13,10 +15,16 @@ public class Individuo implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_individuo;
+    @NotEmpty
     private String nombre;
+    @NotEmpty
     private String apellido;
+
     private String telefono;
+    @NotEmpty
+    @Email
     private String correo;
+    @NotEmpty
     private String edad;
     public Long getId_individuo() {
         return id_individuo;
--- claseCuatro/src/main/java/com/example/segundoProyecto/agregarCRUD/web/Controlador.java
+++ claseCuatro/src/main/java/com/example/segundoProyecto/agregarCRUD/web/Controlador.java
@@ -4,10 +4,12 @@ package com.example.segundoProyecto.agregarCRUD.web;
 
 import com.example.segundoProyecto.agregarCRUD.domain.Individuo;
 import com.example.segundoProyecto.agregarCRUD.services.IndividuoServices;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -39,8 +41,24 @@ public class Controlador {
     }
 
     @PostMapping("/salvar")
-    public String salvar(Individuo individuo) {
+    public String salvar(@Valid Individuo individuo, Errors error) {
+        if (error.hasErrors()) {
+            return "cambiar";
+        }
         individuoServices.salvar(individuo);
+        return "redirect:/";
+    }
+
+    @GetMapping("/cambiar/{id_individuo}")
+    public String cambiar(Individuo individuo, Model model) {
+        individuo = individuoServices.localizarIndividuo(individuo);
+        model.addAttribute("individuo", individuo);
+        return "cambiar";
+    }
+
+    @GetMapping("/borrar")
+    public String borrar(Individuo individuo) {
+        individuoServices.borrar(individuo);
         return "redirect:/";
     }
 }
